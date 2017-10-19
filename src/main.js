4f6785bd8a80ed4eada15ea4c4ea5e5f0d3e41e3
apgyapgy <2648226617@qq.com>
2017-10-19 20:54:51 +0800

    配置初完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
+import VueWechat from 'vue-wechat-title'
+
+import './assets/css/weui.min.css'
+import './assets/css/jquery-weui.min.css'
+import './assets/css/common.css'
+import './assets/js/fastclick.js'
+import './assets/js/jquery-weui.js'
+import './assets/js/common.js'
 
 Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.use(VueWechat);
 
+Vue.http.options.emulateJSON = true;
+Vue.http.interceptors.push((request, next)  => {
+  $.showLoading();
+  request.jsonp = 'callback';
+  next((response)=>{
+  	setTimeout(function(){
+  		$.hideLoading();
+  	});
+  });
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
